3a9818c73f92aa0d31d4e01a963caef8d35c44d7
Farzad Sunavala <40604067+farzad528@users.noreply.github.com>
2021-05-20 10:18:53 -0500

    file struct and pragma import

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
+  
 const DStorage = artifacts.require("DStorage");
 
-module.exports = function (deployer) {
-  //Deploy Contract
+module.exports = function(deployer) {
+  deployer.deploy(DStorage);
 };
--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -1,20 +1,32 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.4.21 <0.6.0;
 
 contract DStorage {
-  // Name
-  string public name = 'DStorage';
-  // Number of files
-  // Mapping fileId=>Struct 
+    // Name
+    string public name = "DStorage";
+    // Number of files
+    // Mapping fileId=>Struct
+    mapping(uint => File) public files;
 
-  // Struct
+    // KEY:1 => VALUE : Qmduasdf907234masdflhsadfljhlsadf
+    // KEY:2 => VALUE : Kbasdf078123ljsad097132123kasdadd
 
+    // Struct
+    struct File {
+        uint256 fileId;
+        string fileHash;
+        uint256 fileSize;
+        string fileType;
+        string fileName;
+        string fileDescription;
+        uint256 uploadTime;
+        address payable uploader;
+    }
 
-  // Event
+    // Event
 
-  constructor() public {
-  }
+    constructor() public {}
 
-  // Upload File function
+    // Upload File function
 
     // Make sure the file hash exists
 
@@ -28,11 +40,9 @@ contract DStorage {
 
     // Make sure file size is more than 0
 
-
     // Increment file id
 
     // Add File to the contract
 
     // Trigger an event
-
 }
